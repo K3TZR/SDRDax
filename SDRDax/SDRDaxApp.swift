c0e90141ac89e04194e16c8e122d2aaa3616331e
Douglas Adams <douglas.adams@me.com>
2024-01-30 15:13:46 -0500

    Initial Commit

---
+++ SDRDax/SDRDaxApp.swift
@@ -0,0 +1,17 @@
+//
+//  SDRDaxApp.swift
+//  SDRDax
+//
+//  Created by Douglas Adams on 1/30/24.
+//
+
+import SwiftUI
+
+@main
+struct SDRDaxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
